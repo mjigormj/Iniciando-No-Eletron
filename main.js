a8a8c1e5a4dc82ed28c8e150b1009f36a741faad
JeffersonSoares99 <jefferson99soares@gmail.com>
2020-03-06 22:11:18 -0300

    app

--- main.js
+++ main.js
@@ -1,41 +1,29 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+var electron = require('electron');
+var app = require('app');
+var BrowserWindow = require('browser-window');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+// referência global para manter a instância da janela até que sejam fechadas pelo usuário então ele irá ser fechado quando o JavaScript fizer Garbage collection
+var mainWindow = null;
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+// Sair da aplicação quando todas as janelas forem fechadas
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
+app.on('ready', function() {
+  // Cria a janela do browser.
+  mainWindow = new BrowserWindow({width: 1200, height: 700});
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+  // Carrega o arquivo html principal.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  // aber o DevTools. (console, inspecionar elemento, etc)
+  // mainWindow.webContents.openDevTools(); 
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  // Evento emitido quando a janela é fechada, usado para destruir instancia.
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
